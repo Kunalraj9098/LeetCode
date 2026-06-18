dc66a7decad2da21dff148144f3f7ac137c899e3
Kunal Raj Patidar <patidarkunalraj19@gmail.com>
2026-06-18 13:20:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.8 MB (61.56%)

---
+++ solutions/1446-angle-between-hands-of-a-clock/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        if (hour==12){
+            hour=0;
+        }
+     double angle=  Math.abs(30.0*hour - 5.5*minutes);
+     return Math.min(360.0-angle, angle) ;  
+     } }   

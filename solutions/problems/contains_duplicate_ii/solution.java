3099eb5a4061d487960ccdb8158f0f9aaef3a83c
Kunal Raj Patidar <patidarkunalraj19@gmail.com>
2026-08-20 06:35:09 +0000

    Sync LeetCode submission - Contains Duplicate II (java)

---
+++ solutions/problems/contains_duplicate_ii/solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution { 
+    public boolean containsNearbyDuplicate(int[] nums, int k) { 
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        
+        for (int i = 0; i < nums.length; i++) { 
+            // Fix 1: Changed .contains() to .containsKey()
+            if (map.containsKey(nums[i])) { 
+                int oldIndex = map.get(nums[i]); 
+                if (i - oldIndex <= k) { 
+                    return true; 
+                } 
+            } 
+            // Fix 2: Changed num[i] to nums[i]
+            map.put(nums[i], i); 
+        } 
+        return false; 
+    } 
+}
